08456f06b4e7062d20fbc6d6aa1253664df82c4a
Karthik.E <karthike1947@gmail.com>
2026-05-02 19:58:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function run() {
+  let input = document.getElementById("input").value;
+
+  // simple demo logic
+  let result = "Processed: " + input;
+
+  document.getElementById("output").innerText = result;
+}
